631fcc07385de78168760aab19e276e11e41829a
dvladir <dragonvladir@gmail.com>
2021-09-26 02:20:46 +0300

    Cleanup

--- partners-db/sql/create-contact-table.sql
+++
@@ -1,9 +0,0 @@
-create table contact (
-    partner_id  uuid primary key,
-    phone varchar(20) default '',
-    email varchar(100) default '',
-    constraint fk_contact_partner
-        foreign key (partner_id)
-            references partner_info(id)
-            on delete cascade
-);
--- partners-db/sql/create-partner-table.sql
+++
@@ -1,5 +0,0 @@
-create type partner_type as enum ('naturalPerson', 'legalEntity');
-create table partner_info (
-    id uuid primary key default gen_random_uuid(),
-    partner_type partner_type not null
-);
